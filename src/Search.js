3e739b5381b179376b479915d3d5a916b1263a47
Your Name <arteevraina@gmail.com>
2019-12-31 22:22:36 +0530

    Update

--- src/Search.js
+++ src/Search.js
@@ -5,27 +5,33 @@ class Search extends React.Component {
     constructor() {
         super()
         this.state = {
-            books: []
+            books: [],
+            query: ''
         }
         this.bind = this.handleClick.bind(this)
+        this.bind = this.handleChange.bind(this)
     }
 
     handleClick() {
-        axios.get('https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyBgLSwNtArchOjB__1-kqOoqrUt1d_AwXA')
-        .then(resonse => {
-            console.log(response => {
-                console.log(response)
-            })
-        })
-        .catch(err => {
-            console.log("Error")
+        const apiPath = "https://www.googleapis.com/books/v1/volumes?q="
+        const apiKey = "AIzaSyBgLSwNtArchOjB__1-kqOoqrUt1d_AwXA"
+        var part = "&key="
+        let query = this.state.query
+        let url = apiPath + query + part + apiKey
+        
+
+    }
+
+    handleChange(event) {
+        this.setState({
+            query: event.target.value
         })
     }
 
     render(){
     return(
         <div class="container">
-            <input type="text" className="form-control" placeholder="Search for Books"/>
+            <input type="text" className="form-control" placeholder="Search for Books" onChange={this.handleChange}/>
             <button type="submit" className="btn btn-success" onClick={this.handleClick}>Search</button>
         </div>
     )}
