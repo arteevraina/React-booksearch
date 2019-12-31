463834b4ebd778c36899f1ee0ad5c4730d6e0eb9
Your Name <arteevraina@gmail.com>
2019-12-31 21:02:10 +0530

    Commit

--- src/Search.js
+++ src/Search.js
@@ -1,13 +1,32 @@
 import React from "react"
+import axios from "axios"
 
 class Search extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            books: []
+        }
+        this.bind = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        axios.get('https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyBgLSwNtArchOjB__1-kqOoqrUt1d_AwXA')
+        .then(resonse => {
+            console.log(response => {
+                console.log(response)
+            })
+        })
+        .catch(err => {
+            console.log("Error")
+        })
+    }
+
     render(){
     return(
-    <div>
         <div class="container">
-        <input type="text" className="form-control" placeholder="Search for Books"/>
-        <button type="submit" className="btn btn-success">Search</button>
-    </div>
+            <input type="text" className="form-control" placeholder="Search for Books"/>
+            <button type="submit" className="btn btn-success" onClick={this.handleClick}>Search</button>
         </div>
     )}
 }
