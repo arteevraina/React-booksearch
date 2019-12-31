9829d041197301d19e737d03b22ca3659e207644
Your Name <arteevraina@gmail.com>
2020-01-01 00:39:43 +0530

    enhanced UI

--- src/Search.js
+++ src/Search.js
@@ -2,24 +2,30 @@ import React from "react"
 import axios from "axios"
 
 class Search extends React.Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
             books: [],
             query: ''
         }
         this.bind = this.handleClick.bind(this)
         this.bind = this.handleChange.bind(this)
+        
     }
 
     handleClick() {
         const apiPath = "https://www.googleapis.com/books/v1/volumes?q="
         const apiKey = "AIzaSyBgLSwNtArchOjB__1-kqOoqrUt1d_AwXA"
         var part = "&key="
-        let query = this.state.query
-        let url = apiPath + query + part + apiKey
-        
-
+        let key = this.state.query
+        var url = apiPath + key + part + apiKey
+        axios.get(url)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(err => {
+                console.log("Error")
+            })
     }
 
     handleChange(event) {
@@ -30,9 +36,13 @@ class Search extends React.Component {
 
     render(){
     return(
-        <div class="container">
-            <input type="text" className="form-control" placeholder="Search for Books" onChange={this.handleChange}/>
-            <button type="submit" className="btn btn-success" onClick={this.handleClick}>Search</button>
+        <div className="container">
+            <h1>Book Search App</h1>
+            <form className="form-group">
+                <input type="text" className="form-control" placeholder="Search for Books" onChange={this.handleChange}/>
+                <br/>
+                <button type="submit" className="btn btn-success" onClick={this.handleClick}>Search</button>
+            </form>
         </div>
     )}
 }
